d44fddc9f37a288b330f91bdad32e6a80e3df9cf
Ashu0Singh <ashutosh.20bci7070@vitap.ac.in>
2022-12-09 02:59:10 +0530

    Polling image Updates

--- src/components/Polling/Poll.js
+++ src/components/Polling/Poll.js
@@ -1,63 +1,64 @@
-import React,{useState,useEffect} from "react";
-import './Poll.css'
-// import Axios from 'axios';
-import Wrapper from '../Wrapper/Wrapper'
+import React, { useState, useEffect } from "react";
+import "./Poll.css";
+import Axios from "axios";
+import Wrapper from "../Wrapper/Wrapper";
 
-export default function Poll(){
-    // const url = "http://3.6.65.227:8080/api/";
-    const [teamDetails , setTeamDetails] = useState({time : 100});
-    const [timer , setTimer] = useState({second : 0, mins : 0});
+export default function Poll() {
+  // const [isPollingStarted, setPollingState] = useState(false);
+  // Axios.get("http://localhost:8080/api/polling").then((response) => {
+  //   setPollingState(response.data.isPollingStarted);
+  // });
 
-    // Axios.get(url+"/polling").then(
-    //     response => {
-    //         setTeamDetails(response)
-    //     }
-    // );
-    useEffect(() => {
-        const timerfunc = () => {
-                                const timeLeft = teamDetails.time - 1;
-                                setTimer({
-                                    second: timeLeft % 60,
-                                    mins : Math.floor(timeLeft /60)
-                                });
-                                setTeamDetails({time : timeLeft});
-                                console.log(teamDetails);
-                            } 
+  const [counter, setCounter] = useState(100);
 
-        setInterval(timerfunc , 1000);
-        return clearInterval(timerfunc , 1000);
-    });
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((prevCounter) => prevCounter - 1);
+    }, 1000);
 
-    const style  = {gap:"1rem" };
-    return (
-        <Wrapper >
-            
-            <div className="flex-col borders" style={{...style,gap:"2rem"}}>
-                <h1 className='fs-800 title fc-white extrabold'>{">Polling"}</h1>
-                {
-                teamDetails.time > 0 ?
-                    <div className="flex-col poll">
-                        <div className="flex-col" style={style}>
-                            <img 
-                                alt='generateImage'
-                                className='poll-displayimage'
-                                src={process.env.PUBLIC_URL + "/assests/capybara/Capybara-1.png"}
-                            />
-                            <button className='button fs-300 extrabold fc-white'>+1</button>
-                        </div>
-                        <h1 className='fs-600 extrabold fc-white'>{`${timer.mins} : ${timer.second}`}</h1>
-                        <div className="flex-col" style={style}>
-                            <img 
-                                alt='generateImage'
-                                className='poll-displayimage'
-                                src={process.env.PUBLIC_URL + "/assests/capybara/Capybara-2.png"}
-                            />
-                            <button className='button fs-300 extrabold fc-white'>+1</button>
-                        </div>
-                    </div>
-                    : <div className="fs-400 fc-white extrabold flex-col poll" style={{height:"100%", justifyContent:"center"}}> Polling will start in sometime </div>
+    return () => clearInterval(interval);
+  }, []);
+
+  const style = { gap: "1rem" };
+  return (
+    <Wrapper>
+      <div className="flex-col borders" style={{ ...style, gap: "2rem" }}>
+        <h1 className="fs-800 title fc-white extrabold">{">Polling"}</h1>
+        {counter > 0 ? (
+          <div className="flex-col poll">
+            <div className="flex-col" style={style}>
+              <img
+                alt="generateImage"
+                className="poll-displayimage"
+                src={
+                  process.env.PUBLIC_URL + "/assests/capybara/Capybara-1.png"
                 }
+              />
+              <button className="button fs-300 extrabold fc-white">+1</button>
             </div>
-        </Wrapper>
-    );
+            <h1 className="fs-600 extrabold fc-white">{` ${Math.floor(
+              counter / 60
+            )} : ${counter % 60}`}</h1>
+            <div className="flex-col" style={style}>
+              <img
+                alt="generateImage"
+                className="poll-displayimage"
+                src={
+                  process.env.PUBLIC_URL + "/assests/capybara/Capybara-2.png"
+                }
+              />
+              <button className="button fs-300 extrabold fc-white">+1</button>
+            </div>
+          </div>
+        ) : (
+          <div
+            className="fs-400 fc-white extrabold flex-col poll"
+            style={{ height: "100%", justifyContent: "center" }}
+          >
+            Polling will start in sometime
+          </div>
+        )}
+      </div>
+    </Wrapper>
+  );
 }
